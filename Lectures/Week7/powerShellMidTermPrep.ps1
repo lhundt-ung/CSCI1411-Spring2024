7c7715deb83aee60021262941bbd64b66810092e
Lance Hundt <lance.hundt@ung.edu>
2024-02-25 09:11:26 -0500

    Update MidtermPrep

--- Lectures/Week7/powerShellMidTermPrep.ps1
+++ Lectures/Week7/powerShellMidTermPrep.ps1
@@ -3,13 +3,31 @@ $logs = Get-WinEvent Security -MaxEvents 50
 
 #2. From the variable in number 1, display all InstanceID's not equal to 5379. 
 # Store results in second variable
-$logs = $logs | Where-Object{$_.Id -ne "5379"}
-$logs.length
+
+$logs = $logs | Where-Object {$_.Id -ne "5379"}
 
 #3. Write a loop that iterates the array variable in #2
 # Print only the InstanceID  for each object that contains the word ADMINISTRATOR in the message
+$count = 0
 foreach($item in $logs){
     if($item.Message -like "*ADMINISTRATOR*"){
-        Write-Host $item.Id
+        Write-Host $item.Message
+        $count++
     }
 }
+
+Write-Host $count
+
+
+function addNumbers($firstNumber,$secondNumber){
+    $sum = $firstNumber + $secondNumber
+    Write-Host $sum
+}
+
+$a = 5
+$b = 10
+
+addNumbers($a,$b)
+addNumbers(5,10)
+addNumbers $a $b
+addNumbers -firstNumber $a -secondNumber $b
